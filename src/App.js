f364b36c3e4dbfe58cd7639343480027fc164ae5
kaskom <48438761+kaskom@users.noreply.github.com>
2019-09-10 15:35:17 +0300

    create header and add header styles

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop.component';
 import {Switch, Route} from 'react-router-dom';
+import Header from './components/header/header.component.jsx';
 
 function App() {
   return (
     <div >
+    	<Header />
     	<Switch>
     		<Route exact path ='/' component = {HomePage} />
-    		<Route exact path = '/shop' component = {ShopPage} />
+    		<Route  path = '/shop' component = {ShopPage} />
 
     	</Switch>
     </div>
